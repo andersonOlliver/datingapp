8b871f78f27ff28c9abdb686b36b0fc774c5ffb1
Anderson Olliver <pla.olliver@gmail.com>
2018-05-01 02:15:19 -0400

    Implemented Paging and sorting member

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BsDropdownModule, TabsModule, DatepickerModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, DatepickerModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
@@ -28,6 +28,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 @NgModule({
   declarations: [
@@ -41,18 +42,21 @@ import { FileUploadModule } from 'ng2-file-upload';
     MemberListComponent,
     ListsComponent,
     MessagesComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TimeAgoPipe
   ],
   imports: [
     AuthModule,
     BrowserModule,
     BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
+    ButtonsModule.forRoot(),
     FileUploadModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
     NgxGalleryModule,
+    PaginationModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     TabsModule.forRoot(),
   ],
